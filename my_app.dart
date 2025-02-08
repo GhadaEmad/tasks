74ad35da9ffcbb0ca6f53fee065d604ccdcd2f30
GhadaEmad <ghadaemad39@gmail.com>
2025-02-08 14:36:26 +0200

    Add files via upload

--- my_app.dart
+++ my_app.dart
@@ -1,16 +1,16 @@
 
 import 'package:flutter/material.dart';
 
-import 'first_scren.dart';
+import 'first_screen.dart';
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner:false ,
-        home: FirstScren()
-    );
+      debugShowCheckedModeBanner: false,
+        home: FirstScreen())
+      ;
   }
-
 }
